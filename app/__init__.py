05cc89d35460c2786aaefec3a24f0a9851e53a35
Matt Arnold <matt.mattarn@gmail.com>
2016-07-03 20:50:03 -0400

    Fix Wsgi for adding virtual environment.

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,9 @@
 from flask import Flask
-from flask.ext.sqlalchemy import SQLAlchemy
+# from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, static_url_path='')
 app.config.from_object('config')
-db = SQLAlchemy(app)
+# db = SQLAlchemy(app)
 
 
 from app.routes import index
--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,6 @@
 import sys
 sys.path.insert(0, '/home/skin/skintosticker')
-#activate_this = '/venv/bin/activate_this.py'
-#execfile(activate_this, dict(__file__=activate_this))
+activate_this = '/home/skin/skintosticker/venv/bin/activate_this.py'
+execfile(activate_this, dict(__file__=activate_this))
 
 from app import app as application
