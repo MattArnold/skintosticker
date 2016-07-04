5833ff04698627f4b9936748eacb6808a1fb0ef4
Matt Arnold <matt.mattarn@gmail.com>
2016-07-03 20:17:00 -0400

    Apache config files.

---
+++ wsgi.py
@@ -0,0 +1,6 @@
+import sys
+sys.path.insert(0, '/home/skin/skintosticker')
+#activate_this = '/venv/bin/activate_this.py'
+#execfile(activate_this, dict(__file__=activate_this))
+
+from app import app as application
